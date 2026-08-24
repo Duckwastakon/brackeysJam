954552523674d48b7cb434a2df077775309bad63
Duckwastakon <vitolsrinalds4@gmail.com>
2026-08-24 16:38:44 +0300

    resources

---
+++ resources/dropped_item.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+const decel = 1
+var startingScale = 1
+
+func _ready() -> void:
+	startingScale = $ColorRect.scale.x
+
+func _physics_process(delta: float) -> void:
+	if(velocity != Vector2.ZERO):
+		velocity.x = move_toward(velocity.x, 0, decel)
+		velocity.y = move_toward(velocity.y, 0, decel)
+		
+		$ColorRect.scale = Vector2(clamp(startingScale * velocity.length(), 1, 2), 
+		clamp(startingScale * velocity.length(), 1, 2))
+		
+		move_and_slide()
--- resources/resource.gd
+++ resources/resource.gd
@@ -3,23 +3,39 @@ extends Area2D
 var resourceId = 0
 signal shake
 
+const resourse = preload("res://resources/dropped_item.tscn")
+
 func _ready() -> void:
 	connect("shake", shakeObject)
+
+func dropResource():
+	var newResource: CharacterBody2D = resourse.instantiate()
+	newResource.global_position = global_position
+	get_parent().call_deferred("add_child", newResource)
+	var x = randi_range(-100, 100)
+	var rand = randi_range(-1, 1)
+	while(rand == 0):
+		rand = randi_range(-1, 1)
+	
+	var y = (100 - abs(x)) * rand
+	newResource.velocity = Vector2(x, y)
 
 var shakeAmount = 0
 
 func shakeObject():
+	dropResource()
+	
 	if shakeAmount <= 0:
-		shakeAmount = 20
+		shakeAmount = 8
 		while(shakeAmount > 0):
 			shakeAmount -= 1
 			var newTween = create_tween()
-			newTween.tween_property($ColorRect, "position", 
-			Vector2(randi_range(-4, 4), randi_range(-4, 4)), 0.04)
+			newTween.tween_property($ColorRect, "position",
+			Vector2(-48 + randi_range(-4, 4), -48 + randi_range(-4, 4)), 0.04)
 			
 			newTween.play()
 			await newTween.finished
 			newTween.kill()
-		$ColorRect.position = Vector2(0, 0)
+		$ColorRect.position = Vector2(-48, -48)
 	else:
-		shakeAmount = 20
+		shakeAmount = 8
