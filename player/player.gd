e09d6bbee6a6a2a91f9ee1bfdf8f20c7b6a9b158
Duckwastakon <vitolsrinalds4@gmail.com>
2026-08-24 14:42:53 +0300

    player wobbling + rotation

--- player/player.gd
+++ player/player.gd
@@ -12,11 +12,16 @@ func _physics_process(delta: float) -> void:
 	if(movementDirection != Vector2.ZERO):
 		velocity += movementDirection * accelerations[movingState]
 		
+		if(movementDirection.x == 0):
+			velocity.x = move_toward(velocity.x, 0, deceleration)
+		if(movementDirection.y == 0):
+			velocity.y = move_toward(velocity.y, 0, deceleration)
 		lockSpeed()
 	else:
 		velocity.x = move_toward(velocity.x, 0, deceleration)
 		velocity.y = move_toward(velocity.y, 0, deceleration)
 	
+	playerWobble()
 	move_and_slide()
 
 func lockSpeed():
@@ -30,3 +35,24 @@ func lockSpeed():
 			velocity.y = move_toward(velocity.y, 0, deceleration)
 		else:
 			velocity.y = velocity.y / abs(velocity.y) * maxSpeed[movingState]
+
+var rotatingState = -1
+@onready var playerArt = $playerArt
+
+func playerWobble():
+	if(velocity != Vector2.ZERO):
+		if (velocity.x > 0):
+			playerArt.flip_h=true
+		else:
+			playerArt.flip_h=false
+		
+		var wobbleSpeed = 1000 / (abs(velocity.x) + abs(velocity.y))
+		var maxWobbleDistance = clampf(2000 / (abs(velocity.x) + abs(velocity.y)) * 15, 5, 15)
+		
+		if(playerArt.rotation_degrees == maxWobbleDistance * rotatingState):
+			rotatingState *= -1
+		
+		playerArt.rotation_degrees = move_toward(playerArt.rotation_degrees, 
+		maxWobbleDistance * rotatingState, wobbleSpeed)
+	else:
+		playerArt.rotation_degrees = move_toward(playerArt.rotation_degrees, 0, 5)
