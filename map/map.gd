1fe077a4c213cfbc31875d0ef85ab2dabed6be3e
NK-1-10 <kanepe.nikola@gmail.com>
2026-08-25 15:14:20 +0300

    map generation and assets

--- map/map.gd
+++ map/map.gd
@@ -3,139 +3,55 @@ extends Node2D
 @onready var tile = $background/tile
 @onready var rec = $resource
 @onready var player = $player
-@onready var area = $background/body
 
-var inOne = false; var inTwo = false;var inThree = false; var inFour =false;var inFive = false; var inSix = false;  var inSeven = false; var inEight = false
-var one = 0; var two = 0; var three = 0; var four = 0; var five = 0; var six = 0; var seven = 0; var eight = 0;
+const chunkSE := 640
+const tileS := 64
+const TilesRow := 10
+const rchance := 0.25
 
-var newTile = tile
-var newRec = rec
-var newBack  ; var newCArea = area
-var first = true
-var sqPosition = Vector2(0.0, 0.0)
-var CurrentsqPosition = Vector2(0.0, 0.0)
+var chunks := {}
+var current_chunk_coord := Vector2i.ZERO
 
-var tilePosition = Vector2(0.0, 0.0)
-var currentPosition = Vector2(0.0, 0.0)
-
-var left = 0 ;
-var up= 0;
-var counter = 1;
-
-
-var number = []
-var chunk = []
-var cArea = []
-var currentChunk
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	first = true
-	generate()
-	currentChunk =  chunk[0]
-	
-func generate():
-	
-	if first:
-		sqPosition = Vector2(0.0, 0.0)
-		first = false
-	else:
-		if inOne:
-			pass
-		elif inTwo:
-			two = two+1
-			sqPosition = Vector2(CurrentsqPosition.x, CurrentsqPosition.y+640)
-			left = 0
-			up = (640)*two
-		elif inThree:
-			pass
-		elif inFour:
-			pass
-		elif inFive: 
-			five = five+1
-			sqPosition = Vector2(CurrentsqPosition.x, CurrentsqPosition.y-640)
-			left = 0
-			up = (-640)*five
-		elif inSix :
-			pass
-		elif inSeven:
-			up = 0;
-			seven = seven + 1
-			sqPosition = Vector2(CurrentsqPosition.x-640, CurrentsqPosition.y)
-			left = (-640)*seven
-		else:
-			up = 0
-			eight = eight + 1
-			sqPosition = Vector2(CurrentsqPosition.x+640, CurrentsqPosition.y)
-			left = (640)*eight
-	
-	
-	newBack = square.duplicate() # needs reworking when addingh get s added
-	newBack.position = sqPosition
-	chunk.append(newBack)
-	add_child(newBack)
-	print(square.position) # square starting position
-	CurrentsqPosition = sqPosition
-	
-	for o in 10:
-		var n = o*64
-		for i in 10 :
-			var y = n - up
-			var a = i * 64 + left
-			newTile = tile.duplicate()
-			newTile.position = Vector2(a, y)
-			add_child(newTile)
-			number.append(newTile)
-			
-	generateRec()
-
-func generateRec():
-	for i in 100:
-		var f = randf() < 0.25
-		if f:
-			newRec = rec.duplicate()
-			add_child(newRec)
-			var which = i*counter
-			print(which)
-			newRec.position = number[which].global_position + Vector2(randi_range(1, 64), randi_range(1, 64))	
-	counter = counter+1	
-
+	generate_chunk(Vector2i.ZERO)
+	_update_chunks()
 
 func _process(delta: float) -> void:
-	pass
+	var player_chunk = Vector2i(
+		floori(player.global_position.x / chunkSE),
+		floori(player.global_position.y / chunkSE)
+	)
+	if player_chunk != current_chunk_coord:
+		current_chunk_coord = player_chunk
+		_update_chunks()
 
+func _update_chunks() -> void:
+	for dx in [-1, 0, 1]:
+		for dy in [-1, 0, 1]:
+			generate_chunk(current_chunk_coord + Vector2i(dx, dy))
 
-func _on_load_body_exited(body: Node2D) -> void: # when exited big guy
-	var place = player.global_position
-	print(place)
-	generate()
-	
-	
-func _on_one_body_entered(body: Node2D) -> void:
-	inOne = true;
-	inTwo = false; inThree = false; inFour = false; inFive = false; inSix = false ;inSeven = false; inEight = false
-func _on_two_body_entered(body: Node2D) -> void:
-	inTwo = true
-	inOne = false; inThree = false; inFour = false; inFive = false; inSix = false ;inSeven = false; inEight = false
-func _on_three_body_entered(body: Node2D) -> void:
-	inThree = true
-	inTwo = false; inOne = false; inFour = false; inFive = false; inSix = false ;inSeven = false; inEight = false
-func _on_four_body_entered(body: Node2D) -> void:
-	inFour = true
-	inTwo = false; inThree = false; inOne = false; inFive = false; inSix = false ;inSeven = false; inEight = false
+func generate_chunk(coord: Vector2i) -> void:
+	if chunks.has(coord):
+		return
+	var origin = Vector2(coord.x * chunkSE, coord.y * chunkSE)
+	var new_back = square.duplicate()
+	new_back.position = origin
+	add_child(new_back)
+	chunks[coord] = new_back
 
-func _on_five_body_entered(body: Node2D) -> void:
-	inFive = true
-	inTwo = false; inThree = false; inFour = false; inOne = false; inSix = false ;inSeven = false; inEight = false
+	var tiles_in_chunk = []
+	for y in TilesRow:
+		for x in TilesRow:
+			var new_tile = tile.duplicate()
+			new_tile.position = origin + Vector2(x * tileS, y * tileS)
+			add_child(new_tile)
+			tiles_in_chunk.append(new_tile)
 
-func _on_six_body_entered(body: Node2D) -> void:
-	inSix = true
-	inTwo = false; inThree = false; inFour = false; inFive = false; inOne = false ;inSeven = false; inEight = false
+	generate_resources(tiles_in_chunk)
 
-func _on_seven_body_entered(body: Node2D) -> void:
-	inSeven = true
-	inTwo = false; inThree = false; inFour = false; inFive = false; inSix = false ;inOne = false; inEight = false
-
-func _on_eight_body_entered(body: Node2D) -> void:
-	inEight = true
-	inTwo = false; inThree = false; inFour = false; inFive = false; inSix = false ;inSeven = false; inOne = false
+func generate_resources(tiles_in_chunk: Array) -> void:
+	for t in tiles_in_chunk:
+		if randf() < rchance:
+			var new_rec = rec.duplicate()
+			add_child(new_rec)
+			new_rec.position = t.global_position + Vector2(randi_range(1, tileS), randi_range(1, tileS))
