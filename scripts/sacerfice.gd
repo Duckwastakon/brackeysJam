bc66699b2da5e4a74604045b4c2a10777532425b
NK-1-10 <kanepe.nikola@gmail.com>
2026-08-27 15:36:18 +0300

    temple

--- scripts/sacerfice.gd
+++ scripts/sacerfice.gd
@@ -2,26 +2,22 @@ extends Node
 @onready var timer = $sacerfice
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 
 func _on_sacerfice_timeout() -> void:
-	if AnimalData.phase ==  5:
+	if AnimalData.phase ==  5: # if phase is at 5, cant go higher
 		timer.start()
 	else:
 		AnimalData.phase += 1
 		timer.start()
-	
 
 func added():
-	if AnimalData.phase ==  1:
+	if AnimalData.phase ==  1: # if the phase is t one, cant go below
 		timer.start()
 	else:
 		AnimalData.phase -= 1
