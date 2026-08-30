a5d23803e05d5e4723555b7fce06293fa7fd4b9b
NK-1-10 <kanepe.nikola@gmail.com>
2026-08-30 12:32:35 +0300

    sound

--- audio.gd
+++ audio.gd
@@ -1,4 +1,11 @@
 extends Node2D
 
-func playSound():
-	var newSfx = AudioStreamPlayer2D.new()
+
+func playSound(sound):
+	var newSfx: AudioStreamPlayer2D = AudioStreamPlayer2D.new()
+	newSfx.stream = load(sound)
+	newSfx.bus = &"SFX"
+	newSfx.play()
+	await newSfx.finished
+	newSfx.queue_free()
+	
